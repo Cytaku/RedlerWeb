555fb33fcee8229e63dc9bc80503159dc48da95e
Redler <Redler@REDLER-PC>
2016-08-19 16:25:49 -0300

    Add project files.

---
+++ RedlerWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RedlerWeb.Startup))]
+namespace RedlerWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
